68e07cc02ffe477ae2b01663b3c56817698d6e2c
Camilo <ocblanco03@ucatolica.edu.co>
2024-09-27 19:47:02 -0500

    Add header and banner fruver

---
+++ script/fruverjs.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+    
+    addToCartButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Producto añadido al carrito');
+        });
+    });
+});
